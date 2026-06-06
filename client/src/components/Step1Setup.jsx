aa176765c7f0d5076a6add2df5ea7e3396bc57a3
Akshit Dimri <akshitdimri31@gmail.com>
2026-06-06 14:27:56 +0530

    interview page

--- client/src/components/Step1Setup.jsx
+++ client/src/components/Step1Setup.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { motion } from "motion/react";
+import React from "react";
+import { motion } from "motion/react";
+import {
+  FaUserTie,
+  FaBriefcase,
+  FaFileUpload,
+  FaMicrophoneAlt,
+  FaChartLine,
+} from "react-icons/fa";
+
 const Step1Setup = ({ onStart }) => {
   return (
     <motion.div
@@ -9,6 +19,39 @@ const Step1Setup = ({ onStart }) => {
       className="min-h-screen flex items-center justify-center bg-grdient-to-br from-gray-100 to-gray-200 px-4"
     >
       <div className="w-full max-w-6xl bg-white rounded-3xl shadow-2xl grid md:grid-cols-2 overflow-hidden">
+        <motion.div
+          initial={{ x: -80, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.7 }}
+          className=" relative items-center bg-grdient-to-br from-green-100 to-gren-200 p-12 flex flex-col justify-center"
+        >
+          <h2 className="text-4xl font-bold text-gray-800 mb-6">
+            Start your Ai Interview
+          </h2>
+
+          <p className="text-gray-600 mb-10">
+            Practice real interview scenarios powered by Ai. <br />
+            Improve communication, technical skills, ans confidence.
+          </p>
+
+          <div className="space-y-5">
+            {[
+              {
+                icon: <FaUserTie className="text-green-600 text-xl" />,
+                text: "Choose Role & Experience",
+              },
+              {
+                icon: <FaMicrophoneAlt className="text-green-600 text-xl" />,
+                text: "Smart Voice Interview",
+              },
+              {
+                icon: <FaChartLine className="text-green-600 text-xl" />,
+                text: "Performance Analytics",
+              },
+            ].map((moveItem,index))}
+          </div>
+        </motion.div>
+
         <motion.div
           initial={{ x: -80, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
