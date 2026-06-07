c16bc804a5e2c643bcc8c3ef10d65088d442cb36
Akshit Dimri <akshitdimri31@gmail.com>
2026-06-07 16:38:47 +0530

    more files

--- client/src/components/Step2Interview.jsx
+++ client/src/components/Step2Interview.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import malevideo from "../assets/Videos/male-ai.mp4";
 import femalevideo from "../assets/Videos/female-ai.mp4";
+import Timer from "../components/Timer";
+import { motion } from "motion/react";
+import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 
 function Step2Interview({ interviewData, onFinish }) {
   const { intervieId, questions, userName } = interviewData;
@@ -23,15 +26,62 @@ function Step2Interview({ interviewData, onFinish }) {
 
           <div className="w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5">
             <div className="flex justify-between items-center">
-              <span className="text-sm text-gray-500">
-                Interview Status
-              </span>
+              <span className="text-sm text-gray-500">Interview Status</span>
               <span className="text-sm font-semibold text-emerald-600">
                 Ai Speaking...
               </span>
             </div>
             <div className="h-px bg-gray-200"></div>
-            <div className="flex justify-center"></div>
+            <div className="flex justify-center">
+              <Timer timeLeft="30" totalTime="60" />
+            </div>
+            <div className="h-px bg-gray-200"></div>
+            <div className="grid-grid-col-2 gap-6 text-center">
+              <div>
+                <span className="text-2xl font-bold text-emerald-600">1</span>
+                <span className="text-xs text-gray-400">current ques</span>
+              </div>
+              <div>
+                <span className="text-2xl font-bold text-emerald-600">5</span>
+                <span className="text-xs text-gray-400">total ques</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* text section */}
+        <div className="flex-1 flex flex-col p-4 sm:p-6 md:p-8 relative">
+          <h2 className="text-xl sm:text-2xl font-bold text-emerald-600 mg-6">
+            Ai Smart Interview
+          </h2>
+          <div className="relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm">
+            <p className="text-xs sm:text-sm text-gray-400 mg-2">
+              Question 1 of 5
+            </p>
+            <div className="text-base sm:text-lg font-semibold text-gray-800 leading-0">
+              {" "}
+              First Question
+            </div>
+
+            <textarea
+              placeholder="type your answer here"
+              name=""
+              id=""
+              className="flex-1 bg-gray-100 p-4 sm:p-6 riunded-2xl resize-none outline-none border border-gray-200 focus-ring-2 focus:ring-emerald-500 transition text-gray-800"
+            ></textarea>
+            <div className="flex items-center gap-4 mt-6">
+              <motion.button
+                whileTap={{ scale: 0.9 }}
+                className="w-12 h-12 sm-w-14 sm-h-14 flex items-center justify-center rounded-full bg-black text-white shadow-lg"
+              >
+                <FaMicrophone size={20} />
+                <motion.button
+                  whileTap={{ scale: 0.9 }}
+                  className="flex-1 bg-linear-to-r from-emerald-600 to to-teal-500 text-white py-3 sm:py-4 rounded-2xl shadow-lg hover:opacity-90 transition font-semibold"
+                >
+                  Submit Answer
+                </motion.button>
+              </motion.button>
+            </div>
           </div>
         </div>
       </div>
